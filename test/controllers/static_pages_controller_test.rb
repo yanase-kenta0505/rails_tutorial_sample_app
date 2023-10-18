74f0903f7252fa9cedd4981426292f336b938169
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-19 08:23:21 +0900

    testコードの修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,6 +16,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
