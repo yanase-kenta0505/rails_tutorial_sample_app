9ccb176a51ae1e56eddd44a6aeb273315cdbecfd
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-24 08:46:14 +0900

    6.32まで対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231023004633) do
+ActiveRecord::Schema.define(version: 20231023233341) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
