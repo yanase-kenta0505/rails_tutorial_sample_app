6f88748eca597777ddb37d20e795859a626bb195
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-10-26 11:22:16 +0900

    8.24まで対応

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     # ログイン用のパスを開く
     get login_path
@@ -21,6 +25,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     # 移動先のページでフラッシュメッセージが表示されていないことを確認する
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
 
