69acd442600762e4b0f4eab7095d6344e172fd90
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-11-01 12:19:37 +0900

    11.38まで対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,16 +28,27 @@ class User < ApplicationRecord
       update_attribute(:remember_digest, User.digest(remember_token))
     end
 
-      # 渡されたトークンがダイジェストと一致したらtrueを返す
-      def authenticated?(attribute, token)
-        digest = send("#{attribute}_digest")
-        return false if digest.nil?
-        BCrypt::Password.new(digest).is_password?(token)
-      end
+    # 渡されたトークンがダイジェストと一致したらtrueを返す
+    def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
+    end
 
     # ユーザーのログイン情報を破棄する
     def forget
-        update_attribute(:remember_digest, nil)
+      update_attribute(:remember_digest, nil)
+    end
+
+    # アカウントを有効にする
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # 有効化用のメールを送信する
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
     end
 
     private
