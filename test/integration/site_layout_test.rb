d62ac1d3d5634db9cf239a32115e17ac85596060
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-11-02 11:18:39 +0900

    13.45まで対応

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -30,7 +30,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
