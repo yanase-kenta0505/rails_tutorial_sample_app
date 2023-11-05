13b5019a665f5957c89919510ab2a68b829404fd
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-11-05 18:54:42 +0900

    14.7まで対応

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
     belongs_to :follower, class_name: "User"
     belongs_to :followed, class_name: "User"
-  end
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
+end
