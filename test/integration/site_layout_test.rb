4bc7f4ce809479602631cc0fde74ee5c8f8b4930
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-11-06 11:39:44 +0900

    演習対応（コーディング）

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -37,4 +37,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_select "title", full_title(@user.name)
   end
+
+  test "root path layout links with logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+  end
 end
