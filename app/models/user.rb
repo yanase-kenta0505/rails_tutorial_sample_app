6ee0c04dc71409c3bda54c181899cacd06238b48
yanase-kenta0505 <ynsknt0505@gmail.com>
2023-11-06 17:03:51 +0900

    14.45まで対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ApplicationRecord
     # 試作feedの定義
     # 完全な実装は次章の「ユーザーをフォローする」を参照
     def feed
-      Micropost.where("user_id = ?", id)
+      Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
     end
 
     # ユーザーをフォローする
